9fa3ce11ca904150c192c4e203ec4d901bd98ea4
Shaun <shaun@thenetninja.co.uk>
2021-01-13 16:11:00 +0000

    lesson-5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,27 +30,25 @@ export default {
 
 <style>
   /* enter transitions */
-  .toast-enter-from {
-    opacity: 0;
-    transform: translateY(-60px);
-  }
-  /* .toast-enter-to {
-    opacity: 1;
-    transform: translateY(0);
-  } */
   .toast-enter-active {
-    transition: all 0.3s ease;
+    animation: wobble 0.5s ease;
   }
   /* leave transitions */
-  /* .toast-leave-from {
-    opacity: 1;
-    transform: translateY(0);
-  } */
   .toast-leave-to {
     opacity: 0;
     transform: translateY(-60px);
   }
   .toast-leave-active {
     transition: all 0.3s ease;
+  }
+
+  @keyframes wobble {
+    0% { transform: translateY(-100px); opacity: 0 }
+    50% { transform: translateY(0px); opacity: 1 }
+    60% { transform: translateX(8px); opacity: 1 }
+    70% { transform: translateX(-8px); opacity: 1 }
+    80% { transform: translateX(4px); opacity: 1 }
+    90% { transform: translateX(-4px); opacity: 1 }
+    100% { transform: translateX(0px); opacity: 1 }
   }
 </style>
