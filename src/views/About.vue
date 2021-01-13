65fd688846696c2b754285d388f47ed8c7a34691
Shaun <shaun@thenetninja.co.uk>
2021-01-13 16:22:23 +0000

    lesson-9

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <transition
+      appear
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -8,10 +19,36 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   setup() {
+    const showTitle = ref(true)
 
+    const beforeEnter = (el) => {
+      console.log('before enter', el)
+    }
+    const enter = (el) => {
+      console.log('enter', el)
+    }
+    const afterEnter = (el) => {
+      el.style.color = 'green'
+      console.log('after enter', el)
+
+      setTimeout(() => showTitle.value = false, 2000)
+    }
+    const beforeLeave = (el) => {
+      el.style.color = "pink"
+      console.log('before leave', el)
+    }
+    const leave = (el) => {
+      console.log('leave', el)
+    }
+    const afterLeave = (el) => {
+      console.log('after leave', el)
+    }
+
+    return { beforeEnter, enter, afterEnter, beforeLeave, leave, afterLeave, showTitle }
   }
 }
 </script>
@@ -20,5 +57,18 @@ export default {
   .about {
     max-width: 600px;
     margin: 20px auto;
+  }
+
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 3s ease;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .fade-leave-active {
+    transition: opacity 3s ease;
   }
 </style>
