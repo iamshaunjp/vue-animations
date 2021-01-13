ae9f1aa2c769292e14e0fdbe91273ee309b75c36
Shaun <shaun@thenetninja.co.uk>
2021-01-13 16:28:27 +0000

    lesson-10

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,9 @@
   <div class="about">
     <transition
       appear
-      name="fade"
       @before-enter="beforeEnter"
       @enter="enter"
-      @after-enter="afterEnter"
-      @before-leave="beforeLeave"
-      @leave="leave"
-      @after-leave="afterLeave"
+      :css="false"
     >
       <h1 v-if="showTitle">About</h1>
     </transition>
@@ -20,35 +16,29 @@
 
 <script>
 import { ref } from 'vue'
+import gsap from 'gsap'
 
 export default {
   setup() {
     const showTitle = ref(true)
 
     const beforeEnter = (el) => {
-      console.log('before enter', el)
+      console.log('before enter - set intial state')
+      el.style.transform = 'translateY(-60px)'
+      el.style.opacity = 0
     }
-    const enter = (el) => {
-      console.log('enter', el)
-    }
-    const afterEnter = (el) => {
-      el.style.color = 'green'
-      console.log('after enter', el)
-
-      setTimeout(() => showTitle.value = false, 2000)
-    }
-    const beforeLeave = (el) => {
-      el.style.color = "pink"
-      console.log('before leave', el)
-    }
-    const leave = (el) => {
-      console.log('leave', el)
-    }
-    const afterLeave = (el) => {
-      console.log('after leave', el)
+    const enter = (el, done) => {
+      console.log('starting to enter - make transition')
+      gsap.to(el, {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+      })
     }
 
-    return { beforeEnter, enter, afterEnter, beforeLeave, leave, afterLeave, showTitle }
+    return { beforeEnter, enter, showTitle }
   }
 }
 </script>
@@ -57,18 +47,5 @@ export default {
   .about {
     max-width: 600px;
     margin: 20px auto;
-  }
-
-  .fade-enter-from {
-    opacity: 0;
-  }
-  .fade-enter-active {
-    transition: opacity 3s ease;
-  }
-  .fade-leave-to {
-    opacity: 0;
-  }
-  .fade-leave-active {
-    transition: opacity 3s ease;
   }
 </style>
