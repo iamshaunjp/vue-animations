7bd55e65de45312d0e51d927bb02a78f36789bc7
Shaun <shaun@thenetninja.co.uk>
2021-01-13 15:57:56 +0000

    lesson-3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <Toast v-if="showToast" />
     <Todos @badValue="triggerToast" />
+    <transition name="fade">
+      <div v-if="showP">hello, ninjas</div>
+    </transition>
+    <button @click="showP = !showP">toggle</button>
   </div>
 </template>
 
@@ -14,17 +18,35 @@ export default {
   components: { Toast, Todos },
   setup() {
     const showToast = ref(false)
+    const showP = ref(false)
 
     const triggerToast = () => {
       showToast.value = true;
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return { showToast, triggerToast }
+    return { showToast, triggerToast, showP }
   }
 }
 </script>
 
 <style>
-
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-to {
+    opacity: 1;
+  }
+  .fade-enter-active {
+    transition: all 2s ease;
+  }
+  .fade-leave-from {
+    opacity: 1;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .fade-leave-active {
+    transition: all 2s ease;
+  }
 </style>
